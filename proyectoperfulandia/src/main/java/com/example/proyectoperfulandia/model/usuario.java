142e83c36789ceb1a934a3503cd49ad21ff86fa8
robert <ro.groom@duocuc.cl>
2025-05-19 10:59:06 -0400

    usuario claseeee

--- proyectoperfulandia/src/main/java/com/example/proyectoperfulandia/model/usuario.java
+++ proyectoperfulandia/src/main/java/com/example/proyectoperfulandia/model/usuario.java
@@ -1,4 +1,9 @@
 package com.example.proyectoperfulandia.model;
 
 public class usuario {
+    private String rut;
+
+    private String name;
+    private String lastName;
+    private String mail;
 }
