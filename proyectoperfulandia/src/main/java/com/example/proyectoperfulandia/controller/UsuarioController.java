b07fd294f3f83d1d012af532879918f387a58241
Robert <ro.groom@duocuc.cl>
2025-05-20 10:44:19 -0400

    avance Usuario

--- proyectoperfulandia/src/main/java/com/example/proyectoperfulandia/controller/UsuarioController.java
+++ proyectoperfulandia/src/main/java/com/example/proyectoperfulandia/controller/UsuarioController.java
@@ -1,4 +1,22 @@
 package com.example.proyectoperfulandia.controller;
 
+import com.example.proyectoperfulandia.model.Usuario;
+import com.example.proyectoperfulandia.services.UsuarioService;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping
+    public String getUsuarios(){
+        return usuarioService.getUsuarios();
+    }
+
+    @GetMapping("/{id}");
+
 }
