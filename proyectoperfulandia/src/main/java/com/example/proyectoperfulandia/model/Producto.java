2b346697ec3cd8fb89db4781555ce358b457f5bc
Robert Groom <ro.groom@duocuc.cl>
2025-05-21 17:46:43 -0400

    Producto

--- proyectoperfulandia/src/main/java/com/example/proyectoperfulandia/model/Producto.java
+++ proyectoperfulandia/src/main/java/com/example/proyectoperfulandia/model/Producto.java
@@ -1,23 +1,33 @@
 package com.example.proyectoperfulandia.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.Data;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 @Entity
-@Table(name = "PRODUCTOS")
+@Table(name = "producto")
 public class Producto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+
+    @Column(unique = true, length = 10, nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
     private String nombre;
+
+    @Column(nullable = false)
     private double precio;
+
+    @Column(nullable = false)
     private int stock;
+
+    @Column(nullable = false)
     private String categoria;
 }
