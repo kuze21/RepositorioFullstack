070f5ba82c58b225645527db2aa060718009529a
Martin Pavez <mart.pavez@duocuc.cl>
2025-06-02 10:58:23 -0400

    carpeta config

---
+++ proyectoperfulandia/src/main/java/com/example/proyectoperfulandia/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+package com.example.proyectoperfulandia.config;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("")
+                        .version("1.0")
+                        .description("Documentacion de la API para el sistema de proyecto")
+    }
+}
