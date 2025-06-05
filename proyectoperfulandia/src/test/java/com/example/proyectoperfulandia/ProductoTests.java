938f48d78073b25e2785756901be69a5098a976c
David García <dav.garcia@duocuc.cl>
2025-06-05 09:43:23 -0400

    Agregados 2 tests

---
+++ proyectoperfulandia/src/test/java/com/example/proyectoperfulandia/ProductoTests.java
@@ -0,0 +1,82 @@
+package com.example.proyectoperfulandia;
+
+import com.example.proyectoperfulandia.model.Producto;
+import com.example.proyectoperfulandia.repository.ProductoRepository;
+import com.example.proyectoperfulandia.services.ProductoService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductoTests {
+
+	@Autowired
+	ProductoRepository productoRepository;
+
+	@MockitoBean
+	ProductoService productoServiceMock;
+
+	@Autowired
+	MockMvc mockMvc;
+
+	/*
+	# Estructura @Test
+	@Test
+	void testFuncion(){
+		# Se puede indicar un valor para suplantar y acotar el resultado que se espera de la funcion.
+		when().thenReturn("");
+
+		# Bloque try/catch permite probar un segmento o funcion y agarrar el error
+		# mockMvc permite consultas HTTPMethod
+		try{
+			mockMvc.perform(get("/"))
+			.andExpect(status().isOk())
+			.andExpect(content().string(""))
+		}
+
+		catch(Exception ex){
+		 System.out.println(ex.getMessage());
+		 fail();
+		}
+	}
+	*/
+	@Test
+	@DisplayName("Test Listar Productos")
+	void testListarProductos(){
+		when(productoServiceMock.listarProducto()).thenReturn("Lista completa");
+		try{
+			mockMvc.perform(get("/productos"))
+					.andExpect(status().isOk())
+					.andExpect(content().string("Lista completa"));
+		}
+
+		catch(Exception ex){
+			System.out.println(ex.getMessage());
+			fail();
+		}
+
+	}
+
+	@Test
+	@DisplayName("Rectificar precio producto")
+	void testFindProduct(){
+		Producto prueba = productoRepository.findById(1).get();
+		assertNotNull(prueba);
+		assertEquals(359990,prueba.getPrecio());
+	}
+
+
+
+
+}
--- proyectoperfulandia/src/test/java/com/example/proyectoperfulandia/ProyectoperfulandiaApplicationTests.java
+++ proyectoperfulandia/src/test/java/com/example/proyectoperfulandia/ProyectoperfulandiaApplicationTests.java
@@ -1,62 +1,12 @@
 package com.example.proyectoperfulandia;
 
-import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-
-//Perdon Robert, puse todos los import porque no me dejaba usar el mockito bien
-
-import com.example.proyectoperfulandia.controller.ProductoController;
-import com.example.proyectoperfulandia.model.Producto;
-import com.example.proyectoperfulandia.repository.ProductoRepository;
-import com.example.proyectoperfulandia.services.ProductoService;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.mockito.Mockito;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.mockito.configuration.IMockitoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class ProyectoperfulandiaApplicationTests {
-
-	@Autowired
-	MockMvc mockMvc;
 
 	@Test
 	void contextLoads(){
 	}
-
-	/*
-	Estructura @Test
-	@Test
-	void testFuncion(){
-		# Se puede indicar un valor para suplantar y acotar el resultado que se espera de la funcion.
-		when().thenReturn("");
-
-		# Bloque try/catch permite probar un segmento o funcion y agarrar el error
-		# mockMvc permite consultas HTTPMethod
-		try{
-			mockMvc.perform(get("/"))
-			.andExpect(status().isOk())
-			.andExpect(content().string(""))
-		}
-
-		catch(Exception ex){
-		 System.out.println(ex.getMessage());
-		 fail();
-		}
-	}
-	*/
 }
